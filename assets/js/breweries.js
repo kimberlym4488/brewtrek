06298c8b704723680df1b366ccafc6116713dcac
cmseibel <cam.m.seib@gmail.com>
2021-12-11 12:51:29 -0800

    refactor script.js across breweries.js and script.js

--- assets/js/breweries.js
+++ assets/js/breweries.js
@@ -5,15 +5,44 @@ var resultsEl = $("#query-results");
 
 /**
  * Writes a list item to be appended to breweries.html
- * @param {*} name name of the brewery
+ * @param {string} name name of the brewery
+ * @param {number} id openbrewerydb id of the brewery
  * @returns a list item with a link to brewery.html
  */
-function writeResult(name){
+function writeResult(name, id){
     // TODO: Add brewery information in the body of the list item.
     return `
     <li class="box">
-        <a href="./brewery.html?q=${name}">
-            PLACEHOLDER TEXT
+        <a href="./brewery.html?q=${id}">
+            ${name}
         </a>
     </li>`;
 }
+
+
+function getBreweries(latitude, longitude) {
+    // Insert the API url to get a list of weather data
+   console.log(`This is my ${longitude} latitude, this is my ${latitude} longitude`)
+
+    var requestUrl = `https://api.openbrewerydb.org/breweries?by_dist=${latitude},${longitude}&page=1`;
+    
+    fetch(requestUrl)
+      .then(async function (response) {
+        var data = await response.json();
+        printMainContainer(data);
+        return;
+      })
+
+    } 
+     
+function printMainContainer(data){
+    //Add contents into daily cards.
+    for (var i = 0; i<data.length; i++) {
+        console.log(data);
+        console.log(data[i].name)
+        console.log(data[i].website_url)
+        resultsEl.append(writeResult(data[i].name, data[i].id));
+    }
+}
+
+getBreweries(params.get("lat"), params.get("lon"));
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,6 @@ var city = document.querySelector("#input");
 var mainText = document.querySelector("#mainText");
 var invalidFeedback = document.querySelector(".feedback");
 var APIkey = "a059151d000029215400bdaa7965fbc2"
-var latitude = '';
-var longitude = '';
 
 
 function displayTime() {
@@ -13,61 +11,27 @@ function displayTime() {
     timeDisplayEl.text(now);
 }
 
-function getBreweries(latitude, longitude) {
-    // Insert the API url to get a list of weather data
-   console.log(`This is my ${longitude} latitude, this is my ${latitude} longitude, this is my ${APIkey} ApiKey. My residence is ${city.value}`)
-
-    var requestUrl = `https://api.openbrewerydb.org/breweries?by_dist=${latitude},${longitude}&page=1`;
-    
-    fetch(requestUrl)
-      .then(async function (response) {
-        var data = await response.json();
-        printMainContainer(data);
-        return;
-        //this object contains all the data we requested. 
-      })
-
-    } 
-     
-function printMainContainer(data){
-
-     //for some reason calling the icon below works ks to get the icon to append to the div above.
-    //Add contents into daily cards.
-    for (var i = 0; i<data.length; i++) {
-        console.log(data);
-        console.log(data[i].name)
-        console.log(data[i].website_url)
-   
-        var htmlTemplate = "";
-
-    htmlTemplate+=`<p>Brewery info ${data[i].name} goes here</p>`
-
-    document.location.replace("./breweries.html");
-   //append string HTML with jQuery
-    }
-}
 
 function getLatLon(){
-    var requestUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city.value}&appid=a059151d000029215400bdaa7965fbc2`;
+    var requestUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city.value}&appid=${APIkey}`;
     
     fetch(requestUrl)
       .then(async function (response) {
         const data = await response.json();
-      for (var i = 0; i<data.length; i++) {
-            console.log(data)
-            latitude = data[i].lat;
-            longitude = data[i].lon;
-            console.log(longitude);
-            console.log(latitude);
-            getBreweries(latitude,longitude);
-            return;
-      }
+        console.log(data)
+        var latitude = data[0].lat;
+        var longitude = data[0].lon;
+        console.log(longitude);
+        console.log(latitude);
+        document.location.replace(`./breweries.html?lat=${latitude}&lon=${longitude}`);
+        // getBreweries(latitude,longitude);
+        return;
         //}
         //can put data here
         
         //this object contains all the data we requested. SHould we have a fail safe if it doesn't work, like try your zip code?
       })
-      };
+};
 
 function buttonClickHandler(event){
     event.preventDefault();
