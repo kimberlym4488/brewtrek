61f7134369d2d06908880506b4edcc7c8b50d1fa
SamusMoran <kimberlym4488@comcast.net>
2021-12-12 18:59:01 -0800

    Centers brewery page text, updates address line formatting. Stuck on dashes in phone number

--- assets/js/brewery.js
+++ assets/js/brewery.js
@@ -4,6 +4,7 @@ var params = new URLSearchParams(document.location.search);
 var query = `https://api.openbrewerydb.org/breweries/${params.get("q")}`;
 var nameEl = $("#brewery-name");
 var addressEl = $("#address");
+var cityStateZipEl = $("#cityStateZip");
 var phoneEl = $("#phone");
 var urlEl = $("#url");
 
@@ -14,12 +15,14 @@ function getBrewery(){
         })
         .then(function(data){
             nameEl.text(data.name);
-            addressEl.text(`${data.street}, ${data.city}, ${data.state} ${data.postal_code}`)
+            addressEl.text(`${data.street}`)
+            cityStateZipEl.text(`${data.city}, ${data.state}  ${data.postal_code}`)
             phoneEl.text(data.phone);
             phoneEl.attr("href", `tel:${data.phone}`);
             urlEl.text(data.website_url);
             urlEl.attr("href", data.website_url);
-        })
+            }
+        )
 }
 
 getBrewery();
