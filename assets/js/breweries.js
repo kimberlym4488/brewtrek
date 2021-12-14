66b011ba663f46f3eb1115516acb0246934d95d2
cmseibel <cam.m.seib@gmail.com>
2021-12-13 20:12:20 -0800

    Write link to google maps.

--- assets/js/breweries.js
+++ assets/js/breweries.js
@@ -30,32 +30,12 @@ function writeResult(name, id){
 // subtitleEle.append(SubtitleEle);
 
 // column2Ele.append(rowEle);
-return 
-`<div class="tile is-ancestor">
-    <div class="tile is-parent">
+return`
+<div class="tile is-parent is-3">
     <article class="tile is-child box">
-        <p class="title">${One}</p>
-        <p class="subtitle">${Subtitle}</p>
+        <p class="title">${name}</p>
+        <p class="subtitle">${id}</p>
     </article>
-    </div>
-    <div class="tile is-parent">
-    <article class="tile is-child box">
-        <p class="title">${Two}</p>
-        <p class="subtitle">Subtitle</p>
-    </article>
-    </div>
-    <div class="tile is-parent">
-    <article class="tile is-child box">
-        <p class="title">${Three}</p>
-        <p class="subtitle">${Subtitle}</p>
-    </article>
-    </div>
-    <div class="tile is-parent">
-    <article class="tile is-child box">
-        <p class="title">${Four}</p>
-        <p class="subtitle">${Subtitle}</p>
-    </article>
-    </div>
 </div>`
 }
 
--- assets/js/brewery.js
+++ assets/js/brewery.js
@@ -4,6 +4,7 @@ var params = new URLSearchParams(document.location.search);
 var query = `https://api.openbrewerydb.org/breweries/${params.get("q")}`;
 var nameEl = $("#brewery-name");
 var addressEl = $("#address");
+var googleLink = $("#lmgtfy");
 var cityStateZipEl = $("#cityStateZip");
 var phoneEl = $("#phone");
 var urlEl = $("#url");
@@ -16,13 +17,19 @@ function getBrewery(){
         .then(function(data){
             nameEl.text(data.name);
             addressEl.text(`${data.street}`)
-            cityStateZipEl.text(`${data.city}, ${data.state}  ${data.postal_code}`)
+            // googleLink.attr("href", `https://www.google.com/maps/place/${data.street}+${data.city}+${data.state}+${data.postal_code}`)
+            googleLink.attr("href", `https://www.google.com/maps/place/${data.name.split(" ").join("+")}+${data.street}+${data.city}+${data.state}+${data.postal_code}`)
+            cityStateZipEl.text(`${data.city}, ${data.state}  ${data.postal_code}`);
             phoneEl.text(data.phone);
             phoneEl.attr("href", `tel:${data.phone}`);
             urlEl.text(data.website_url);
             urlEl.attr("href", data.website_url);
             }
         )
+}
+
+function delimitPhonenumber(number){
+
 }
 
 getBrewery();
