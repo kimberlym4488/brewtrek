6cfdab44adfcf96cb3e01f8427f216da4914b6e0
cmseibel <cam.m.seib@gmail.com>
2021-12-18 11:59:08 -0800

    Write comments for brewery.js

--- assets/js/brewery.js
+++ assets/js/brewery.js
@@ -10,6 +10,9 @@ var phoneEl = $("#phone");
 var urlEl = $("#url");
 var favoriteBtn = $("#favorite");
 
+/**
+ * Retrieves and displays information for the selected brewery.
+ */
 function getBrewery(){
     fetch(query)
         .then(function(result){
@@ -34,13 +37,18 @@ function getBrewery(){
 		});
 }
 
+/**
+ * Splits up a phone number such that it is more readable.
+ * @param {string or number} number a string (or number) of at least 10 characters representing a phone number.
+ * @returns {string} in the format (###) ###-#### or +## (###) ###-#### depending on whether or not a country code is available.
+ */
 function delineatePhoneNumber(number){
 	number = "" + number;
 	number = number.split("")
 	// OpenBreweryDB might not actually include country codes smh.
 	var countryCode = "";
 	if(number.length > 10){
-		countryCode += number.slice(0, number.length - 10).join("");
+		countryCode += "+" + number.slice(0, number.length - 10).join("");
 	}
 	var rootNumber = number.slice(number.length - 10, number.length);
 	rootNumber.splice(6, 0, "-");
@@ -50,6 +58,9 @@ function delineatePhoneNumber(number){
 	return (countryCode + " " + rootNumber.join("")).trim();
 }
 
+/**
+ * Adds the current brewery to a list of favorite breweries.
+ */
 function setFavorite(){
 	var favorites = JSON.parse(localStorage.getItem("favorites"));
 	var brewery = {
@@ -65,44 +76,54 @@ function setFavorite(){
   window.location.reload();
 }
 
+/**
+ * Displays a list of favorite breweries.
+ */
 $(".favoritesButton").on ("click", function(event){
   event.preventDefault();
   $(".modal").addClass("is-active");
   console.log($(".modal"));
 })
 
+/**
+ * Closes the favorite breweries list.
+ */
 $(".modal-close").on("click", function(event){
   event.preventDefault();
   $(".modal").removeClass("is-active");
    console.log($(".modal"));
 })
 
+/**
+ * Retrieve the current list of favorites.
+ * @returns when the list of favorites is empty.
+ */
 function getFavorites(){
-
-var favoritesList=
-JSON.parse(localStorage.getItem("favorites"));
-console.log(favoritesList)
-//if favoritesList.length===0 {} or !favoritesList.length
-    if (favoritesList===null){
-    return;
-    }
-
-    for(var i=0; i<favoritesList.length;i++){
-    console.log(favoritesList[i].name);
-var viewFavorites=
-    `
-    <tr class = p-3 onclick="openWin(${favoritesList[i].id})">
-      <td class="p-3 tableData" id="tableData">${favoritesList[i].name} 
-      </td>
-    </tr>
-    `
-$(".favoritesTab").append(viewFavorites)
-  }
+	var favoritesList=
+	JSON.parse(localStorage.getItem("favorites"));
+	console.log(favoritesList)
+	//if favoritesList.length===0 {} or !favoritesList.length
+	if (favoritesList===null){
+		return;
+	}
+	for(var i=0; i<favoritesList.length;i++){
+		console.log(favoritesList[i].name);
+		var viewFavorites=
+`
+<tr class = p-3 onclick="openWin(${favoritesList[i].id})">
+	<td class="p-3 tableData" id="tableData">${favoritesList[i].name}</td>
+</tr>`
+		$(".favoritesTab").append(viewFavorites)
+	}
 }
 
 function openWin(){
   console.log("You clicked the favorite")
 }
+
+/**
+ * 
+ */
 function startFacts() {
     $(".funFacts").empty();
     fetch('https://uselessfacts.jsph.pl/random.json?language=en')
@@ -118,25 +139,22 @@ function startFacts() {
       })
 }
 
-function uselessFacts(){
-    
+function uselessFacts(){    
     setInterval(function(){
-        
-             var requestUrl = 'https://uselessfacts.jsph.pl/random.json?language=en'
-             $(".funFacts").empty();
+		var requestUrl = 'https://uselessfacts.jsph.pl/random.json?language=en'
+		$(".funFacts").empty();
 
-             fetch(requestUrl)
-               .then(function (response) {
-                 return response.json();
-               })
-               .then(function (data) {
-                 var htmlTemplate = `
-                 <p>${data.text}<p>`
-                 $(".funFacts").append(htmlTemplate);
-               })
-         }
-       ,10000);
-    }
+		fetch(requestUrl)
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			var htmlTemplate = `
+			<p>${data.text}<p>`
+			$(".funFacts").append(htmlTemplate);
+		})
+	}, 10000);
+}
 
 startFacts();
 getBrewery();
